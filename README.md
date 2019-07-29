984b481ec3c86487ed89d2c0d91509aa41e69088
Anthony Cameron <ar.cameron@gmail.com>
2019-07-29 14:59:14 -0400

    some tweaks

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-1. git clone git@github.com:qq99/dotfiles.git ~/.dotfiles
-2. cd ~/.dotfiles
-3. ./install.sh
+1. Install zsh
+2. Install autojump 
+3. Install oh-my-zsh
+4. git clone git@github.com:qq99/dotfiles.git ~/.dotfiles, or ln -s it there
+5. cd ~/.dotfiles
+6. ./install.sh
+
+https://github.com/powerline/fonts
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,13 @@ alias la='ls -A'
 alias l='ls -CF'
 alias be="bundle exec"
 alias s="tig status"
+alias ss="git status"
 alias gc="git commit"
 alias gco="git checkout"
 alias reload!='. ~/.zshrc'
+alias zshrc='vim ~/.zshrc'
+alias zshreload='. ~/.zshrc'
+alias ffs='dev down && dev up && dev server'
+alias R='. ~/.zshrc'
+alias fixcam='sudo killall VDCAssistant'
+
